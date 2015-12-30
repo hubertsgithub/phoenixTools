9574085c9932897b0f8f78a7f85c54cf732c1251
Balazs Kovacs <bkovacs@cs.cornell.edu>
2015-12-30 02:32:39 -0500

    Added more workers, start and stop container, int verbose

--- phoenixJobs.py
+++ phoenixJobs.py
@@ -18,7 +18,8 @@
 MACHINE_PREFIX = 'phoenix'
 
 # id following prefix:
-MACHINE_IDS = [0]
+MACHINE_IDS = range(0, 11) + [13, 19, 20, 23, 24, 25, 26]
+#MACHINE_IDS = [10]
 MACHINE_ID_FORMAT = '%02d'
 
 # suffix following id:
@@ -37,6 +38,8 @@ TMUX_SESSION = 'celery'
 TMUX_HISTORY_LIMIT = '8000'
 # If the tmux session already exists, kill that before starting another
 KILL_EXISTING = True
+# If this is true, we also stop the container when "stop" command is called
+STOP_CONTAINER = True
 # If true, we don't start more threads than the cpu count - current load
 LOAD_LIMIT = False
 # The commands which will be executed in the docker container for the different options
@@ -73,16 +76,21 @@ def qs(s):
         return ''
 
 
-def run_remotely(machine_name, ssh_cmd, verbose=False):
+def run_remotely(machine_name, ssh_cmd, verbose=0):
     SSH_CMD_PREFIX = 'ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=30'
     cmd = '%s %s %s' % (SSH_CMD_PREFIX, machine_name, qs(ssh_cmd))
-    if verbose:
+    if verbose > 1:
         print 'Running command: "%s"...' % cmd
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    return process.communicate()[0]
+    output, errors = process.communicate()
+    if errors:
+        print 'STDERR:', errors
+
+    return output
 
 
-def run_on_docker(machine_name, image_id, session_cmd, tmux_session, tmux_history_limit, kill_existing=True, verbose=False):
+def run_on_docker(machine_name, container_id, session_cmd, tmux_session,
+                  tmux_history_limit, kill_existing=True, verbose=0):
     start_cmd_list = []
     # Empty command means that we want to kill the existing process
     if kill_existing or not session_cmd:
@@ -96,17 +104,19 @@ def run_on_docker(machine_name, image_id, session_cmd, tmux_session, tmux_histor
         ]
     docker_cmd = '; '.join(start_cmd_list)
 
-    ssh_cmd = 'docker exec -d --privileged --user=ubuntu %s zsh -c "%s"' % (image_id, docker_cmd)
+    ssh_cmd = 'docker exec -d --privileged --user=ubuntu %s zsh -c "%s"' % (container_id, docker_cmd)
+    # TODO: Super hack because of docker bug?
+    #ssh_cmd = 'docker exec -ti --privileged --user=ubuntu %s script -q -c "%s" /dev/null' % (container_id, docker_cmd)
     return run_remotely(machine_name, ssh_cmd, verbose)
 
 
-def get_image_id(machine_name, image_name, verbose=False):
+def get_container_id(machine_name, image_name, verbose=0):
     ssh_cmd = 'docker ps | grep %s | awk \'{print $1}\'' % image_name
     output = run_remotely(machine_name, ssh_cmd, verbose)
     return output.strip()
 
 
-def get_mem_stats(machine_name, verbose=False):
+def get_mem_stats(machine_name, verbose=0):
     '''Gets used memory in megabytes on a certain machine'''
     ssh_cmd = 'free -m | grep + | awk \'{print $3 " " ($3+$4)}\''
     mem_stats = run_remotely(machine_name, ssh_cmd, verbose)
@@ -117,7 +127,7 @@ def get_mem_stats(machine_name, verbose=False):
     return used_memory, all_memory
 
 
-def get_cpu_stats(machine_name, verbose=False):
+def get_cpu_stats(machine_name, verbose=0):
     '''Gets free memory in megabytes on a certain machine'''
     ssh_cmd = 'uptime | sed -r "s/^.*load average: (.*$)/\\1/" | tr "," " " | awk \'{print $1}\''
     load = float(run_remotely(machine_name, ssh_cmd, verbose))
@@ -127,6 +137,18 @@ def get_cpu_stats(machine_name, verbose=False):
         print 'Load on %s: %.2f/%d' % (machine_name, load, cpu_num)
 
     return load, cpu_num
+
+
+def start_container(machine_name, image_name, root_path, verbose=0):
+    ssh_cmd = 'docker pull %s; cd %s; docker run -t -i -d -v /lib/modules:/lib/modules -v $PWD:/host --net=host --privileged %s zsh' % (
+        image_name, qd(root_path), image_name,
+    )
+    return run_remotely(machine_name, ssh_cmd, verbose)
+
+
+def stop_container(machine_name, container_id, verbose=0):
+    ssh_cmd = 'docker stop %s' % container_id
+    return run_remotely(machine_name, ssh_cmd, verbose)
 
 
 def main(args):
@@ -147,21 +169,23 @@ def main(args):
         machine_name = format_str % (MACHINE_PREFIX, mid, MACHINE_SUFFIX)
         print 'Executing on %s...' % machine_name
 
-        image_id = get_image_id(machine_name, IMAGE_NAME, args.verbose)
+        container_id = get_container_id(machine_name, IMAGE_NAME, args.verbose)
 
-        if not image_id:
+        # Stop the container if we have to and it's running
+        if STOP_CONTAINER and not docker_cmd and container_id:
+            stop_container(machine_name, container_id, args.verbose)
+            container_id = get_container_id(machine_name, IMAGE_NAME, args.verbose)
+
+        if not container_id:
             # If we wanted to kill it but it's not even running, we are done
             if not docker_cmd:
                 continue
 
-            print 'Docker image "%s" is not running, starting...' % IMAGE_NAME
-            ssh_cmd = 'docker pull %s; cd %s; docker run -t -i -d -v /lib/modules:/lib/modules -v $PWD:/host --net=host --privileged %s zsh' % (
-                IMAGE_NAME, qd(ROOT_PATH), IMAGE_NAME,
-            )
-            run_remotely(machine_name, ssh_cmd, args.verbose)
+            print 'Docker container "%s" is not running, starting...' % IMAGE_NAME
+            start_container(machine_name, IMAGE_NAME, ROOT_PATH, args.verbose)
 
-            image_id = get_image_id(machine_name, IMAGE_NAME, args.verbose)
-            if not image_id:
+            container_id = get_container_id(machine_name, IMAGE_NAME, args.verbose)
+            if not container_id:
                 print 'Failed to start docker container... Skipping this machine.'
                 continue
 
@@ -181,15 +205,16 @@ def main(args):
 
         subs_dic = {'machine_name': machine_name, 'thread_count': max_thread_count}
         subs_docker_cmd = docker_cmd.format(**subs_dic)
-        run_on_docker(machine_name, image_id, subs_docker_cmd, TMUX_SESSION, TMUX_HISTORY_LIMIT, KILL_EXISTING, args.verbose)
+        run_on_docker(
+            machine_name, container_id, subs_docker_cmd, TMUX_SESSION,
+            TMUX_HISTORY_LIMIT, KILL_EXISTING, args.verbose
+        )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--op', required=True, choices=['start', 'stop'])
-    parser.add_argument('--verbose', dest='verbose', action='store_true')
-    parser.add_argument('--no-verbose', dest='verbose', action='store_false')
-    parser.set_defaults(verbose=False)
+    parser.add_argument('--verbose', type=int, default=0)
     args = parser.parse_args()
 
     main(args)
